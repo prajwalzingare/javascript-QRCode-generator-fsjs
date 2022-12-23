f7253850fcfaa8243b3b1e333584a0fb107b5c14
prajwal zingare <prajwalzingre@gmail.com>
2022-12-23 11:57:40 +0530

    loading functionality added

--- App.js
+++ App.js
@@ -2,10 +2,15 @@ let btn = document.querySelector(".submit");
 let main = document.querySelector(".main");
 let qrInput = document.querySelector(".main input");
 let qrImg = document.querySelector(".qr-code img");
+const loader = document.getElementById("loader");
+
+console.log(qrImg.src);
 let preValue;
 btn.addEventListener("click", () => {
   let qrValue = qrInput.value.trim();
-
+  qrImg.classList.toggle("hidden"); // on ➡️ Off turnIntoOn to 'Off'
+  loader.classList.toggle("hidden"); // Off ➡️ On turnIntoOff to 'On'
+  console.log(qrValue);
   if (!qrValue || preValue === qrValue) return;
   preValue = qrValue;
   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
@@ -15,4 +20,8 @@ btn.addEventListener("click", () => {
     main.classList.add("active");
     btn.innerText = "Generate QR Code";
   });
+  setTimeout(() => {
+    loader.classList.toggle("hidden"); // on ➡️ Off turnIntoOn to 'Off'
+    qrImg.classList.toggle("hidden"); // Off ➡️ On turnIntoOff to 'On'
+  }, 2000);
 });
